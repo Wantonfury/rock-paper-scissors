bec6bbf3e6278387e500d6c5ec99db222c09336d
Wantonfury <orzan.alexandru94@gmail.com>
2022-06-28 02:32:30 +0300

    Finish logic and basic GUI
    Finished the coding necessary and fixed an issue where justify-items
    was used instead of justify-content in a flexbox.
    
    Also fixed an issue where clicking on a button would result in the
    appropriate data-selection value, however, if the user clicked on the
    image inside the button it would result in NaN.
    
    Rearranged variables and added a simple init function to keep things
    neat and tidy.

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -3,13 +3,20 @@
  * 1 - paper
  * 2 - scissors
  */
+
 const PLAYER_ID = 0;
-const AI_ID = 1;
+const PLAYER_VICTORY = 0;
 
-let score = [0, 0];
-let logData = [];
+const COMPUTER_ID = 1;
+const COMPUTER_VICTORY = 1;
 
-let idToChoice = id => {
+const DRAW = 2;
+
+const btn = document.querySelectorAll('#game-buttons button');
+const scores = document.querySelectorAll('#results .results-score');
+const logContent = document.querySelector('#results-log h3');
+
+let selectionToWeapon = id => {
     switch (id) {
         case 0: return 'Rock';
         case 1: return 'Paper';
@@ -19,23 +26,58 @@ let idToChoice = id => {
 
 let computerPlay = () => Math.floor(Math.random() * 3);
 
+let log = str => {
+    logContent.textContent = str;
+}
+
+let decideRound = (result, winnerWeapon, loserWeapon) => {
+    let message = '';
+    
+    switch (result) {
+        // Player wins
+        case PLAYER_VICTORY:
+            message = "You win!";
+            scores[PLAYER_ID].textContent = +scores[PLAYER_ID].textContent + 1;
+            break;
+        
+        // AI wins
+        case COMPUTER_VICTORY:
+            message = "You lose!";
+            scores[COMPUTER_ID].textContent = +scores[COMPUTER_ID].textContent + 1;
+            break;
+        
+        // Draw
+        default:
+            message = "It's a draw!";
+            break;
+    }
+    
+    if (result != DRAW) message += " " + winnerWeapon + ' beats ' + loserWeapon + ".";
+    log(message);
+}
+
 let playRound = e => {
-    let playerSelection = e.target.dataset.selection === null ? e.target.parentNode.dataset.selection : e.target.dataset.selection;
+    let playerSelection = +e.target.dataset.selection;
     let computerSelection = computerPlay();
     
-    if (Math.abs(playerSelection - computerSelection) == 1) ;
-    else ;
-    
-    console.log(playerSelection);
+    if (playerSelection == computerSelection) {
+        decideRound(DRAW, selectionToWeapon(playerSelection), selectionToWeapon(computerSelection));
+    }
+    else if (Math.abs(playerSelection - computerSelection) == 1) {
+        if (playerSelection > computerSelection)
+            decideRound(PLAYER_VICTORY, selectionToWeapon(playerSelection), selectionToWeapon(computerSelection));
+        else decideRound(COMPUTER_VICTORY, selectionToWeapon(computerSelection), selectionToWeapon(playerSelection));
+    }
+    else {
+        if (playerSelection < computerSelection)
+            decideRound(PLAYER_VICTORY, selectionToWeapon(playerSelection), selectionToWeapon(computerSelection));
+        else decideRound(COMPUTER_VICTORY, selectionToWeapon(computerSelection), selectionToWeapon(playerSelection));
+    }
 }
 
 let init = () => {
-    const btn = document.querySelectorAll('#game-buttons button');
-    const scores = document.querySelectorAll('#results h3');
-    const logList = document.querySelectorAll('#results-log li');
-    
     scores.forEach(s => s.textContent = 0);
-    logList.forEach(l => l.textContent = '');
+    logContent.textContent = '';
     
     btn.forEach(b => {
         b.addEventListener('click', playRound);
